9ff34ed57628d20c90be516884064c4560ce0954
ItsNotGoodName <gurnaindeol@gmail.com>
2023-02-07 23:36:53 -0800

    fix: MediaFileFind types

--- lib/rpc/MediaFileFind.ts
+++ lib/rpc/MediaFileFind.ts
@@ -1,50 +1,59 @@
 import { RPCBase } from "../rpcBase";
 
+export type FindFileCondition = {
+  Channel: number;
+  Dirs: string[];
+  Types: ["dav" | "jpg"];
+  Order: "Ascent";
+  Redundant: "Exclusion";
+  Events: null | string[];
+  StartTime: string;
+  EndTime: string;
+  Flags: string[];
+};
+
 export function MediaFileFind(this: RPCBase) {
   return {
     create: (subClassID?: number) => {
-      return this.send("mediaFileFind.factory.create", void 0, {
-        subClassID: subClassID,
-      }).then(function (a) {
+      return this.send<unknown, number>(
+        "mediaFileFind.factory.create",
+        void 0,
+        {
+          subClassID: subClassID,
+        }
+      ).then(function (a) {
         return a.result;
       });
     },
-    findFile: (
-      object: number,
-      condition: {
-        Channel: number;
-        Dirs: string[];
-        Types: Array<"dav" | "jpg">;
-        Order: "Ascent";
-        Redundant: "Exclusion";
-        Events: unknown;
-        StartTime: string;
-        EndTime: string;
-        Flags: string[];
-      }
-    ) => {
+    findFile: (object: number, condition: FindFileCondition) => {
       return this.send(
         "mediaFileFind.findFile",
         { condition: condition },
         { object: object }
       );
     },
-    findNextFile: (object: string, count: number) => {
+    findNextFile: (object: number, count: number) => {
       return this.send<{
         found: number;
         infos: Array<{
           Channel: number;
           StartTime: string;
           EndTime: string;
-          Type: string;
           Length: number;
+          Type: "dav" | "jpg";
           FilePath: string;
           Duration: number;
           Disk: number;
           VideoStream: string;
           Flags: Array<string>;
           Events: Array<string>;
-        }>;
+          Cluster?: number;
+          Partition?: number;
+          PicIndex?: number;
+          Repeat?: number;
+          WorkDir?: string;
+          WorkDirSN?: number;
+        }> | null;
       }>(
         "mediaFileFind.findNextFile",
         { count: count },
@@ -53,14 +62,14 @@ export function MediaFileFind(this: RPCBase) {
         return a.params;
       });
     },
-    getCount: (object: string) => {
+    getCount: (object: number) => {
       return this.send<{ count: number }>("mediaFileFind.getCount", void 0, {
         object: object,
       }).then(function (a) {
         return a.params.count;
       });
     },
-    setQueryResultOptions: (object: string, offset: number) => {
+    setQueryResultOptions: (object: number, offset: number) => {
       return this.send(
         "mediaFileFind.setQueryResultOptions",
         { options: { offset: offset } },
@@ -69,10 +78,10 @@ export function MediaFileFind(this: RPCBase) {
         return a.result;
       });
     },
-    close: (object: string) => {
+    close: (object: number) => {
       return this.send("mediaFileFind.close", void 0, { object: object });
     },
-    destroy: (object: string) => {
+    destroy: (object: number) => {
       return this.send("mediaFileFind.destroy", void 0, { object: object });
     },
   };
