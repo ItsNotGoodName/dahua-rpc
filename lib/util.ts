6cdeee33eed0ee677e3beadf6a7193d436b2f358
ItsNotGoodName <gurnaindeol@gmail.com>
2023-02-07 23:38:40 -0800

    feat: timestamp and file condition to util

--- lib/util.ts
+++ lib/util.ts
@@ -1,8 +1,10 @@
-export function dayInBitmap(bitmap: number, day: number) {
+import type { FindFileCondition } from "./rpc/MediaFileFind";
+
+export function dayInBitmap(bitmap: number, day: number): boolean {
   return !!((bitmap >>> 1) & (1 << (day - 1)));
 }
 
-export function daysFromBitmap(bitmap: number, maxDays = 32) {
+export function daysFromBitmap(bitmap: number, maxDays = 32): number[] {
   const days = [];
   for (let day = 1; day <= maxDays; day++) {
     if ((bitmap >>> 1) & (1 << (day - 1))) {
@@ -11,4 +13,66 @@ export function daysFromBitmap(bitmap: number, maxDays = 32) {
   }
 
   return days;
+}
+
+export function toTimestamp(date: Date): string {
+  return date.toLocaleString("sv-SE");
+}
+
+export function fromTimestamp(timestamp: string): Date {
+  const offset = 0 - new Date().getTimezoneOffset() / 60;
+  return new Date(
+    timestamp.substring(0, 10) +
+      "T" +
+      timestamp.substring(11) +
+      ".000" +
+      (offset > 0 ? "+" : "-") +
+      Math.abs(offset).toString().padStart(2, "0") +
+      ":00"
+  );
+}
+
+export function createFindFileCondition(
+  a: "All" | "Timing" | "Event" | "Alarm" | "Manual" | "Picture",
+  i: { Channel: number; Dirs: string[]; StartTime: string; EndTime: string }
+): FindFileCondition {
+  const condition: FindFileCondition = {
+    Channel: i.Channel,
+    StartTime: i.StartTime,
+    EndTime: i.EndTime,
+    Dirs: i.Dirs,
+    Redundant: "Exclusion",
+    Order: "Ascent",
+    Types: ["dav"],
+    Flags: [""],
+    Events: null,
+  };
+
+  switch (a) {
+    case "All":
+      condition.Types = ["dav"];
+      condition.Flags = ["Timing", "Event", "Event", "Manual"];
+      break;
+    case "Timing":
+      condition.Types = ["dav"];
+      condition.Flags = ["Timing"];
+      break;
+    case "Event":
+      condition.Types = ["dav"];
+      condition.Flags = ["Event"];
+      break;
+    case "Alarm":
+      condition.Types = ["dav"];
+      condition.Flags = ["Event"];
+      break;
+    case "Manual":
+      condition.Types = ["dav"];
+      condition.Flags = ["Manual"];
+      break;
+    case "Picture":
+      condition.Types = ["jpg"];
+      condition.Flags = ["Timing", "Event", "Event"];
+  }
+
+  return condition;
 }
--- test/util.spec.ts
+++ test/util.spec.ts
@@ -1,20 +1,39 @@
 import { expect } from "chai";
-import { dayInBitmap, daysFromBitmap } from "../lib/util";
+import {
+  toTimestamp,
+  dayInBitmap,
+  daysFromBitmap,
+  fromTimestamp,
+} from "../lib/util";
 
 describe("util", function () {
-  const bitmap = 250;
-  const days = [1, 3, 4, 5, 6, 7];
+  describe("bitmap", function () {
+    const bitmap = 250;
+    const days = [1, 3, 4, 5, 6, 7];
 
-  it("dayInBitmap", function () {
-    expect(dayInBitmap(bitmap, 2)).to.be.false;
-    expect(dayInBitmap(bitmap, 20)).to.be.false;
-    expect(dayInBitmap(bitmap, 31)).to.be.false;
-    for (const day of days) {
-      expect(dayInBitmap(bitmap, day)).to.be.true;
-    }
+    it("dayInBitmap", function () {
+      expect(dayInBitmap(bitmap, 2)).to.be.false;
+      expect(dayInBitmap(bitmap, 20)).to.be.false;
+      expect(dayInBitmap(bitmap, 31)).to.be.false;
+      for (const day of days) {
+        expect(dayInBitmap(bitmap, day)).to.be.true;
+      }
+    });
+
+    it("daysFromBitmap", function () {
+      expect(daysFromBitmap(bitmap)).deep.eq(days);
+    });
   });
 
-  it("daysFromBitmap", function () {
-    expect(daysFromBitmap(bitmap)).deep.eq(days);
+  describe("timestamp", function () {
+    [
+      "2023-02-06 00:00:00",
+      "2023-02-06 03:09:09",
+      "2023-02-06 23:59:59",
+    ].forEach((timestamp) => {
+      it(timestamp, function () {
+        expect(toTimestamp(fromTimestamp(timestamp))).to.be.eq(timestamp);
+      });
+    });
   });
 });
